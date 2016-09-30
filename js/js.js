00390a88491ba9c0141cb1084d96b5577917f8c9
Enrique Urbina <btace1347@yahoo.com>
2016-09-30 14:40:57 -0500

    Rearranged components

--- js/js.js
+++ js/js.js
@@ -4,3 +4,15 @@ $("#owl-clients").owlCarousel({
   paginationSpeed : 400,
   autoPlay: 5000
 });
+
+$('body').scrollspy({ target: '#navbar-example' })
+
+
+
+$(document).on('click', 'a', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 1000);
+});
