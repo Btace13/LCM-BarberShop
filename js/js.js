65fb07edd53f00bacc77aa5963aa8528bb981ddb
Enrique Urbina <Btace1347@yahoo.com>
2016-10-02 02:42:51 -0500

    took off smooth scroll for function purposes

--- js/js.js
+++ js/js.js
@@ -1,18 +1,18 @@
 $("#owl-clients").owlCarousel({
-  items : 4,
-  slideSpeed : 300,
-  paginationSpeed : 400,
-  autoPlay: 5000
+    items: 4,
+    slideSpeed: 300,
+    paginationSpeed: 400,
+    autoPlay: 5000
 });
 
-$('body').scrollspy({ target: '#navbar-example' })
 
 
 
-$(document).on('click', 'a', function(event){
-    event.preventDefault();
 
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 1000);
-});
+// $(document).on('click', 'a', function(event) {
+//     event.preventDefault();
+//
+//     $('html, body').animate({
+//         scrollTop: $($.attr(this, 'href')).offset().top
+//     }, 1000);
+// });
